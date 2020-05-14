40843aea5d78104fc41933aba0b69b341cc067e5
Sergiu <Sergiu@LAPTOP-UH2BJPH9>
2020-05-14 19:08:22 +0300

    Add project files.

---
+++ Clasa Stiva/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Clasa_Stiva
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool t = true;
+            string comanda;
+            int n;
+            Console.Write("Introduceti dimensiunea stivei:");
+            n = int.Parse(Console.ReadLine());
+            Stiva STIVA = new Stiva(n);
+            while(t==true)
+            {
+                Console.WriteLine("Optiuni:");
+                Console.WriteLine("1)Push");
+                Console.WriteLine("2)Pop");
+                Console.WriteLine("3)Clear");
+                Console.WriteLine("4)Iesire");
+                comanda = Console.ReadLine();
+
+                if(comanda=="1")
+                {
+                    Console.Write("Introduceti numarul:");
+                    int nr = int.Parse(Console.ReadLine());
+                    STIVA.Push(nr);
+                }
+                if(comanda=="2")
+                {
+                    STIVA.Pop();
+                }
+                if(comanda=="3")
+                {
+                    STIVA.Clear();
+                }
+                if(comanda=="4")
+                {
+                    t = false;
+                }
+            }
+            
+            
+
+        }
+    }
+}
---
+++ Clasa Stiva/Stiva.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clasa_Stiva
+{
+    class Stiva
+    {
+        private int size,i;
+        private int[] v;
+      public Stiva(int n)
+      {
+            size = n;
+            v = new int[n];
+      }
+
+        public void Push(int nr)
+        {
+            for(i=size-1;i>=0;i--)
+            {
+                if (v[i] == 0) { v[i] = nr; break; }
+                if(i==0)
+                Console.WriteLine("Stiva este plina!");
+            }
+        }
+
+        public void Pop()
+        {
+            for(i=0;i<size; i++)
+            {
+                if (v[i] != 0) { Console.WriteLine("{0}", v[i]); break; }
+                if(i==size-1)
+                    Console.WriteLine("Stiva este goala!");
+            }      
+        }
+        public void Clear()
+        {
+            for (i = 0; i < size; i++)
+                v[i] = 0;
+        }
+       
+    }
+}
